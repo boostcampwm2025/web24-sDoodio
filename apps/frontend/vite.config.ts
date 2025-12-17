6a8eee585b992de288ba25182c88dfa212272ade
2020026635 <dnwjd99@hanyang.ac.kr>
2025-12-17 22:07:08 +0900

    chore: 간단하게 PWA 설정 추가

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,12 +1,43 @@
 import path from 'node:path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler', {}]],
+      },
+    }),
+    VitePWA({
+      injectRegister: null,
+      manifest: {
+        name: 'web24-boostcamp',
+        short_name: 'web24',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#09090b',
+        theme_color: '#09090b',
+        icons: [
+          {
+            src: '/vite.svg',
+            sizes: 'any',
+            type: 'image/svg+xml',
+            purpose: 'any',
+          },
+        ],
+      },
+      workbox: {
+        mode: 'development',
+        importScripts: ['push-handlers.js'],
+        navigateFallback: '/index.html',
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+        skipWaiting: true,
+      },
+      devOptions: {
+        enabled: true,
       },
     }),
   ],
