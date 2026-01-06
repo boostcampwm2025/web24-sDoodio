36ccfc230ac910e49b5c78875cef0dd08392544d
2020026635 <dnwjd99@hanyang.ac.kr>
2026-01-06 21:52:10 +0900

    feat: 콘솔에 Log 출력하는 기능 추가

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionLoggingFilter } from './common/logger/http-exception.filter';
+import { LoggingInterceptor } from './common/logger/logging.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor());
+  app.useGlobalFilters(new HttpExceptionLoggingFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
