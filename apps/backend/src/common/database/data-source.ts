d5aa0c6e6c087c1c85d1886860a4e53831f9ba0e
2020026635 <dnwjd99@hanyang.ac.kr>
2026-01-06 20:50:47 +0900

    refactor: database 폴더 common 하위로 이동

--- apps/backend/src/common/database/data-source.ts
+++ apps/backend/src/common/database/data-source.ts
@@ -15,7 +15,7 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  entities: [path.join(__dirname, '..', '**', '*.entity.{ts,js}')],
+  entities: [path.join(__dirname, '..', '..', '**', '*.entity.{ts,js}')],
   migrations: [path.join(__dirname, 'migrations', '*.{ts,js}')],
   synchronize: false,
   logging: !isProd,
--- apps/backend/src/common/database/migrations/1767700194082-auto.ts
+++ apps/backend/src/common/database/migrations/1767700194082-auto.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Auto1767699802337 implements MigrationInterface {
-  name = 'Auto1767699802337';
+export class Auto1767700194082 implements MigrationInterface {
+  name = 'Auto1767700194082';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
