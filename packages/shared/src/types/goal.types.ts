0761f1d54054c95a12432626c9b0c7df5b8ea2fa
2020026635 <dnwjd99@hanyang.ac.kr>
2026-01-07 18:58:48 +0900

    feat: 목표 색상 Enum 추가

---
+++ packages/shared/src/types/goal.types.ts
@@ -0,0 +1,12 @@
+export enum GoalColors {
+  LightPink = '#e4b2b9',
+  Pink = '#e48b84',
+  Yellow = '#edc06a',
+  Sand = '#d7d1ab',
+  Mint = '#abd7b9',
+  Blue = '#abd1d7',
+  GrayMint = '#8ba49d',
+  WarmGray = '#b6aea8',
+  Beige = '#d7c1ab',
+  Lavender = '#c9bae3',
+}
--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -1,1 +1,2 @@
 export * from './some.types';
+export * from './goal.types';
