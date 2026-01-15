a1ef086b881da3f9f59587ec2e202fa3c7c45e26
2020026635 <dnwjd99@hanyang.ac.kr>
2026-01-15 21:54:17 +0900

    chore: CLOVA_API_KEY env에 추가
    Co-authored-by: Yeeun Lee <iyeeun@users.noreply.github.com>

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -24,6 +24,7 @@ import { AIModule } from './features/ai/ai.module';
         DB_USER: Joi.string().required(),
         DB_PASS: Joi.string().required(),
         DB_NAME: Joi.string().required(),
+        CLOVA_API_KEY: Joi.string().required(),
       }),
     }),
     TypeOrmModule.forRootAsync({
