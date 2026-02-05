a4934570922eb7f24fd1be55b8b312815ac17841
KangNaewon <kang.naewon125@gmail.com>
2026-02-05 14:55:56 +0900

    migration: fk 인덱스 생성

---
+++ apps/backend/src/common/database/migrations/1770270383316-auto.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Auto1770270383316 implements MigrationInterface {
+  name = 'Auto1770270383316';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE INDEX "idx_stat_event_logs_user_id" ON "stat_event_logs" ("userId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "idx_push_subscriptions_user_id" ON "push_subscriptions" ("userId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "idx_today_behaviors_user_id" ON "today_behaviors" ("userId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "idx_today_behaviors_behavior_id" ON "today_behaviors" ("behaviorId") `,
+    );
+    await queryRunner.query(`CREATE INDEX "idx_behaviors_goal_id" ON "behaviors" ("goalId") `);
+    await queryRunner.query(`CREATE INDEX "idx_goals_user_id" ON "goals" ("userId") `);
+    await queryRunner.query(
+      `CREATE INDEX "idx_dodo_chat_messages_user_id" ON "dodo_chat_messages" ("userId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "idx_ai_behaviors_user_id" ON "ai_behaviors" ("userId") `,
+    );
+    await queryRunner.query(
+      `CREATE INDEX "idx_ai_behaviors_goal_id" ON "ai_behaviors" ("goalId") `,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX "public"."idx_ai_behaviors_goal_id"`);
+    await queryRunner.query(`DROP INDEX "public"."idx_ai_behaviors_user_id"`);
+    await queryRunner.query(`DROP INDEX "public"."idx_dodo_chat_messages_user_id"`);
+    await queryRunner.query(`DROP INDEX "public"."idx_goals_user_id"`);
+    await queryRunner.query(`DROP INDEX "public"."idx_behaviors_goal_id"`);
+    await queryRunner.query(`DROP INDEX "public"."idx_today_behaviors_behavior_id"`);
+    await queryRunner.query(`DROP INDEX "public"."idx_today_behaviors_user_id"`);
+    await queryRunner.query(`DROP INDEX "public"."idx_push_subscriptions_user_id"`);
+    await queryRunner.query(`DROP INDEX "public"."idx_stat_event_logs_user_id"`);
+  }
+}
